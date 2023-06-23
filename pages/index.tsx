95b6256e76326b77c8e7ae3927d51e60e060ca5a
Aleksandar <17598649+seniorescobar@users.noreply.github.com>
2023-06-23 23:11:53 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const IndexPage = () => (
         </h3>
         <h1 className="text-7xl lg:text-9xl font-extrabold">We’r blowing up</h1>
         <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
-          We're under construction. Check back for an update soon. Stay in
+          We`re under construction. Check back for an update soon. Stay in
           touch!
         </p>
       </div>
